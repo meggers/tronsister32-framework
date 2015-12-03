e7e81752e59a67164f4edd5a1765a28c6801ab68
Max Eggers <eggers.max@gmail.com>
2015-12-03 17:16:10 -0600

    add load single sprite

--- operating_system.asm
+++ operating_system.asm
@@ -2,20 +2,15 @@
 
 x_mask: .word 0xFF000000
 y_mask: .word 0x000000FF
+sprite_index_mask: .word 0x00FF0000
 
 oam_copy: .space 64
 
 .text
 
-li $a0,middle_finger_index
-li $a1,middle_finger_height
-li $a2,middle_finger_width
-li $a3,0
-call load_sprite
-
 #####################################
 #                                   #
-# FUNCTION: load_sprite             #
+# FUNCTION: load_sprite_img         #
 #                                   #
 # DEFN: load sprite into oam        #
 #                                   #
@@ -26,7 +21,7 @@ call load_sprite
 #   $a3: start oam index            #
 #                                   #
 #####################################
-load_sprite:                        #
+load_sprite_img:                    #
     li $t0,0                        # $t0 = 0 ; outer loop index (height)
     li $t1,0                        # $t1 = 0 ; inner loop index (width)
     li $t3,0                        # cur_sprite = 0
@@ -54,6 +49,26 @@ load_sprite:                        #
                                     #
     load_sprite_return:             #
         ret                         #
+#####################################
+
+#####################################
+#
+# 
+#
+#####################################
+load_single_sprite:                 #
+    pop $t0                         # pop oam index
+    pop $t1                         # pop sprite index
+                                    #
+    sft $t0,$t1                     # store in real oam
+                                    #
+    lw $t3,$zero,sprite_index_mask  # load sprite index mask
+    sll $t1,$t1,24                  # shift index to correct spot
+    and $t1,$t1,$t3                 # mask off index
+    sw $t1,$at,0                    # store index at spot in memory
+    addi $at,$at,1                  # increment free oam spot in memory
+    ret                             #
+                                    #
 #####################################
 
 # handle game tick interrupt
